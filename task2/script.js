c4d9d6f41c082cfe280609bab38850ef2e47da14
Timur Nabiullin <timurkre@gmail.com>
2022-12-13 17:22:10 +0600

    three commit

--- task2/script.js
+++ task2/script.js
@@ -3,9 +3,3 @@ const consoleLog = document.querySelector('#consoleLog');
 consoleLog.addEventListener('click',() => {
     alert('Служит для вывода информации в консоль')
 })
-
-const alert = document.querySelector('#alert');
-
-consoleLog.addEventListener('click',() => {
-    alert('позволяет вывести какую-либо информацию во всплывающем окне в браузере')
-})
